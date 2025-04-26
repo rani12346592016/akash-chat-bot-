6ac186de2b6e6f5f5691f6d28c69514dfc65e3f0
rani12346592016 <ariyan1238da@gmail.com>
2025-04-26 17:05:49 +0600

    Update ｢File｣__Info.js

--- Script/commands/｢File｣__Info.js
+++ Script/commands/｢File｣__Info.js
@@ -1,11 +1,46 @@
-‎❀★━━━━━━━━━━━━━━━━━★❀
-‎ ☞︎︎︎𝐏𝐫𝐞𝐟𝐢𝐱 𝐈𝐧𝐟𝐨☜︎︎︎
-‎ ❀★━━━━━━━━━━━━━━━━━★❀
-‎ ❥︎ 𝐁𝐎𝐒𝐒: 𝐔𝐋𝐋𝐀𝐇
-‎ ❥︎ 𝗣𝗿𝗲𝗳𝗶𝘅: [ . ]
-‎ ❥︎ 𝗢𝘄𝗻𝗲𝗿: 𝐀𝐊𝐀𝐒𝐇-𝐁𝐎𝐒𝐒
-‎
-‎❥︎ 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: www.facebook.com/profile.php?id=100090438467997
-‎
-‎
-‎ 𝗧𝗵𝗮𝗻𝗸 𝗬𝗼𝘂 𝗙𝗼𝗿 𝗨𝘀𝗶𝗻𝗴 𝐌𝐞🥰
+const axios = require("axios");
+const request = require("request");
+const fs = require("fs-extra");
+const moment = require("moment-timezone");
+
+module.exports.config = {
+    name: "admin",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "akash",
+    description: "Show Owner Info",
+    commandCategory: "info",
+    usages: "",
+    cooldowns: 5
+};
+
+module.exports.run = async function({ api, event }) {
+    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+
+    var callback = () => api.sendMessage({
+        body: `
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
+┣━━━━━━━━━━━━━━━━━━━━━┫
+┃ 👤 𝐍𝐚𝐦𝐞      : 𝐀𝐊𝐀𝐒𝐇-𝐁𝐎𝐒𝐒
+┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
+┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐈𝐧 𝐂𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝
+┃ 🎂 𝐀𝐠𝐞       : 18
+┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
+┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐝𝐢𝐩𝐥𝐨𝐦𝐚 𝐢𝐧 𝐀𝐠𝐫𝐢𝐜𝐮𝐥𝐭𝐮𝐫𝐞
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐒𝐚𝐭𝐤𝐡𝐢𝐫𝐚, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+┣━━━━━━━━━━━━━━━━━━━━━┫
+┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : ullash01
+┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : [𝐉𝐨𝐢𝐧 𝐍𝐨𝐰](@akash_72963)
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://www.facebook.com/profile.php?id=100090438467997&mibextid=ZbWKwL)
+┣━━━━━━━━━━━━━━━━━━━━━┫
+┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞: 24/04/2025 11:58:34 PM
+┗━━━━━━━━━━━━━━━━━━━━━┛
+        `,
+        attachment: fs.createReadStream(__dirname + "/cache/1.png")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+  
+    return request(encodeURI(`https://graph.facebook.com/100089660775474/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
+        .on('close', () => callback());
+};
